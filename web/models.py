25f124a5faf8ab3370b609d387b4a505e0e26d14
Farzaneh Kamalou <f70.kamalou@gmail.com>
2020-08-30 15:48:43 +0430

    income in models and some beatification

--- web/models.py
+++ web/models.py
@@ -8,3 +8,15 @@ class Expanse(models.Model):
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{} : {} - {}".format(self.text, self.date, self.amount)
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{} : {} - {}".format(self.text, self.date, self.amount)
